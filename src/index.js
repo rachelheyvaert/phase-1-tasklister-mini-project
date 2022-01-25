48a7f7867ca737934174d791b379cc84d744bb26
Rachel Heyvaert <rachelheyvaert@gmail.com>
2022-01-25 07:17:09 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+  })
+
 });
+
+function buildToDo(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+   btn.textContent = '  x '
+   p.textContent = `${todo}`
+   p.appendChild(btn)
+    console.log(p)
+   document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+
+
